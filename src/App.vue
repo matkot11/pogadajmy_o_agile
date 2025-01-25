4bfdafb7c1c3fc4352116f3ac838acb5078197c8
Mateusz Kocik <matkot11@gmail.com>
2025-01-25 11:07:11 +0100

    feature: Add navigation

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,11 @@ import HomeSection from '@/Sections/HomeSection.vue'
 import EpisodesSection from '@/Sections/EpisodesSection.vue'
 import FooterSection from '@/Sections/FooterSection.vue'
 import AboutSection from '@/Sections/AboutSection.vue'
+import NavigationBar from '@/components/NavigationBar.vue'
 </script>
 
 <template>
+  <NavigationBar />
   <HomeSection />
   <AboutSection />
   <EpisodesSection />
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+const handleScroll = (className: string) => {
+  const elementScrollY =
+    document.querySelector(className)!.getBoundingClientRect().top + window.scrollY
+  const offset = -20
+
+  window.scrollBy({ top: elementScrollY + offset - window.scrollY, behavior: 'smooth' })
+}
+</script>
+
+<template>
+  <nav class="navigation-bar">
+    <button class="navigation-bar__button" @click="() => handleScroll('.about-section')">
+      O NAS
+    </button>
+    <button class="navigation-bar__button" @click="() => handleScroll('.episodes-section')">
+      OSTATNIE ODCINKI
+    </button>
+  </nav>
+</template>
+
+<style scoped lang="scss">
+@use '@/assets/typography';
+
+.navigation-bar {
+  padding: var(--space-12);
+  display: flex;
+  justify-content: center;
+  gap: var(--space-24);
+  background-color: var(--color-black);
+  position: sticky;
+  top: 0;
+
+  &__button {
+    @extend %noto-sans-medium;
+    font-size: var(--space-14);
+    color: var(--color-white);
+    text-decoration: none;
+    background-color: var(--color-black);
+    border: none;
+  }
+}
+</style>
