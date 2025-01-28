cfaf27647de8aa65ddc7e7a705275dca12e04e48
Mateusz Kocik <matkot11@gmail.com>
2025-01-28 16:25:23 +0100

    feature: Add responsiveness to footer

--- src/Sections/FooterSection.vue
+++ src/Sections/FooterSection.vue
@@ -6,41 +6,45 @@ import { IconName } from '@/typings/iconName.ts'
 <template>
   <footer class="footer-section">
     <div class="footer-section__logo-wrapper">
-      <GenericIcon :name="IconName.footerLogo" size="150" />
-      <span class="footer-section__copyright"
-        >&#169; 2025 Pogadajmy O Agile. All Rights Reserved.</span
-      >
-    </div>
-
-    <hr class="footer-section__divider" />
-
-    <div class="footer-section__socials-wrapper">
-      <h3>SŁUCHAJ NAS</h3>
-      <div class="footer-section__icons-wrapper">
-        <a href="https://m.youtube.com/@pogadajmyoagile" target="_blank">
-          <GenericIcon :name="IconName.youtube" size="40" />
-        </a>
-        <a
-          href="https://open.spotify.com/show/0yYMHIsmd6I5dGXU3xyIcL?si=22efb5324637422b"
-          target="_blank"
-        >
-          <GenericIcon :name="IconName.spotify" size="40" />
-        </a>
+      <div class="footer-section__logo">
+        <GenericIcon :name="IconName.footerLogo" />
+        <span class="footer-section__copyright">
+          &#169; 2025 Pogadajmy O Agile. All Rights Reserved.
+        </span>
       </div>
+
+      <hr class="footer-section__divider" />
     </div>
 
     <div class="footer-section__socials-wrapper">
-      <h3>SKONTAKTUJ SIĘ Z NAMI</h3>
-      <div class="footer-section__icons-wrapper">
-        <a href="https://www.facebook.com/PogadajmyoAgile" target="_blank">
-          <GenericIcon :name="IconName.facebook" size="40" />
-        </a>
-        <a href="mailto:pogadajmyoagile@gmail.com" target="_blank">
-          <GenericIcon :name="IconName.gmail" size="40" />
-        </a>
-        <a href="https://www.linkedin.com/company/pogadajmy-o-agile/" target="_blank">
-          <GenericIcon :name="IconName.linkedin" size="40" />
-        </a>
+      <div class="footer-section__socials">
+        <h3>SŁUCHAJ NAS</h3>
+        <div class="footer-section__icons-wrapper">
+          <a href="https://m.youtube.com/@pogadajmyoagile" target="_blank">
+            <GenericIcon :name="IconName.youtube" size="40" />
+          </a>
+          <a
+            href="https://open.spotify.com/show/0yYMHIsmd6I5dGXU3xyIcL?si=22efb5324637422b"
+            target="_blank"
+          >
+            <GenericIcon :name="IconName.spotify" size="40" />
+          </a>
+        </div>
+      </div>
+
+      <div class="footer-section__socials">
+        <h3>SKONTAKTUJ SIĘ Z NAMI</h3>
+        <div class="footer-section__icons-wrapper">
+          <a href="https://www.facebook.com/PogadajmyoAgile" target="_blank">
+            <GenericIcon :name="IconName.facebook" size="40" />
+          </a>
+          <a href="mailto:pogadajmyoagile@gmail.com" target="_blank">
+            <GenericIcon :name="IconName.gmail" size="40" />
+          </a>
+          <a href="https://www.linkedin.com/company/pogadajmy-o-agile/" target="_blank">
+            <GenericIcon :name="IconName.linkedin" size="40" />
+          </a>
+        </div>
       </div>
     </div>
   </footer>
@@ -56,27 +60,61 @@ import { IconName } from '@/typings/iconName.ts'
   background-color: var(--color-blackish);
   color: var(--color-white);
 
+  @media only screen and (min-width: 550px) {
+    flex-direction: row;
+    justify-content: space-between;
+  }
+
   &__logo-wrapper {
+    display: flex;
+    flex-direction: column-reverse;
+
+    @media only screen and (min-width: 550px) {
+      flex-direction: row;
+    }
+  }
+
+  &__logo {
     display: flex;
     flex-direction: column;
     align-items: center;
+    justify-content: center;
+
+    svg {
+      width: 180px;
+      height: 130px;
+    }
   }
 
   &__copyright {
     @extend %oswald-regular;
-    font-size: var(--font-8);
+    font-size: var(--font-10);
   }
 
   &__divider {
     border-top: 1px solid var(--color-white);
     margin: var(--space-32) 0;
+
+    @media only screen and (min-width: 550px) {
+      border-left: 1px solid var(--color-white);
+      margin: 0 0 0 70px;
+    }
   }
 
   &__socials-wrapper {
+    display: flex;
+    flex-direction: column;
+
+    @media only screen and (min-width: 700px) {
+      flex-direction: row;
+      gap: 80px;
+    }
+  }
+
+  &__socials {
     @extend %oswald-bold;
     display: flex;
     flex-direction: column;
-    justify-content: space-between;
   }
 
   &__icons-wrapper {
